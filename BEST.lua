a1e5c58e50cd4976c7687e2ed0d236e19275bcc4
nazarkus <nazarkuus@gmail.com>
2026-02-02 16:20:06 +0200

    Update BEST.lua

--- BEST.lua
+++ BEST.lua
@@ -17,22 +17,35 @@ end
 if valid then
     print('You are in whitelist!')
     
-     --если в вайтлисте - запускаем скрипты
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/wartycoon/refs/heads/main/logger.lua", true))()
+    -- [ИСПРАВЛЕНО]: Убрано 'refs/heads/' из всех ссылок
     
+    -- Запускаем логгер (с защитой от ошибок pcall)
+    pcall(function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/wartycoon/main/logger.lua", true))()
+    end)
+    
+    -- Фоновый цикл (BEST2)
     local function backgroundLoop()
         while true do
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/kick/refs/heads/main/BEST2.lua", true))()
+            pcall(function()
+                loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/kick/main/BEST2.lua", true))()
+            end)
             task.wait(10)
         end
     end
     spawn(backgroundLoop)
     
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpgAircraft/refs/heads/main/easy.lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpg/refs/heads/main/easy.lua"))()
+    -- Остальные скрипты (ссылки исправлены)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpgAircraft/main/easy.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/rpg/main/easy.lua"))()
+    
+    -- Эти ссылки и так были рабочие
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FilteringEnabled/NamelessAdmin/main/Source"))()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/infammo/refs/heads/main/infammo.lua"))()
+    
+    -- Ссылка исправлена
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/nazarkus/infammo/main/infammo.lua"))()
+    
     pcall(function() 
         game:GetService("ReplicatedStorage").ACS_Engine.Events.FDMG:Destroy() 
     end)
